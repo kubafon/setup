0e83b0bbc927ce07d214b6ff9a4313f9bf29a9bb
Jakub Wolak <jakub.wolak@outlook.com>
2013-07-12 21:27:29 +0000

    credentials cashe

--- setup.sh
+++ setup.sh
@@ -41,3 +41,5 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+# git cashes credentials not to log in every time
+git config --global credential.helper cache
